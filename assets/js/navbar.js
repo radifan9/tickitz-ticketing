380b0eb571ab6d46898025e80fee7b121dc93ce1
radifan9 <radif.aghnadiin@gmail.com>
2025-07-25 07:47:08 +0700

    feat: (no3) added localStorage redirect and user profile on navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,20 +1,70 @@
 "use strict";
 
+const navContainer = document.querySelector(".nav-container");
+const navBarHorizontal = document.querySelector(".navbar-horizontal");
+const navBarDrop = document.querySelector(".navbar-drop");
+const home = document.querySelector(".home");
+const movie = document.querySelector(".movie");
+const buyTicket = document.querySelector(".buy-ticket");
+const signIn = document.querySelector(".sign-in");
+const signUp = document.querySelector(".sign-up");
+const signOut = document.querySelector(".sign-out");
+const profilePic = document.querySelector(".profile-pic");
+const profileClicked = document.querySelector(".profile-clicked");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
-hamburgerMenu.addEventListener("click", () => {
-  const navContainer = document.querySelector(".nav-container");
-  const navBar = document.querySelector(".navbar");
-  const home = navBar.querySelector(".home");
-  const movie = navBar.querySelector(".movie");
-  const buyTicket = navBar.querySelector(".buy-ticket");
-  const signIn = navBar.querySelector(".sign-in");
-  const signUp = navBar.querySelector(".sign-up");
 
-  // navContainer.classList.add
-  navBar.classList.toggle("nav-drop-down");
-  home.classList.toggle("drop-down");
-  movie.classList.toggle("drop-down");
-  buyTicket.classList.toggle("drop-down");
-  signIn.classList.toggle("drop-down");
-  signUp.classList.toggle("drop-down");
+console.log(hamburgerMenu);
+
+profilePic.addEventListener("click", () => {
+  profileClicked.classList.toggle("hidden");
 });
+
+signOut.addEventListener("click", () => {
+  window.localStorage.removeItem("activeUser");
+  window.location.reload();
+});
+
+hamburgerMenu.addEventListener("click", () => {
+  // navBar.classList.toggle("nav-drop-down");
+  console.log("Masuk hamburger");
+  navBarHorizontal.classList.toggle("hidden");
+  navBarDrop.classList.toggle("hidden");
+
+  const home = navBarDrop.querySelector(".home");
+  const movie = navBarDrop.querySelector(".movie");
+  const buyTicket = navBarDrop.querySelector(".buy-ticket");
+  const signIn = navBarDrop.querySelector(".sign-in");
+  const signUp = navBarDrop.querySelector(".sign-up");
+
+  console.log(home);
+
+  home.classList.toggle("hidden");
+  movie.classList.toggle("hidden");
+  buyTicket.classList.toggle("hidden");
+  signIn.classList.toggle("hidden");
+  signUp.classList.toggle("hidden");
+
+  home.classList.toggle("drop-down-block");
+  movie.classList.toggle("drop-down-block");
+  buyTicket.classList.toggle("drop-down-block");
+  signIn.classList.toggle("drop-down-block");
+  signUp.classList.toggle("drop-down-block");
+});
+
+if (localStorage.getItem("activeUser")) {
+  signIn.classList.remove("drop-down-block");
+  signUp.classList.remove("drop-down-block");
+  signIn.classList.add("hidden");
+  signUp.classList.add("hidden");
+
+  let userLocalStorage = JSON.parse(localStorage.getItem("user1"));
+  const profileName = document.querySelector(".profile-name");
+  profileName.classList.toggle("hidden");
+  const email = profileName.querySelector("p");
+  email.textContent = userLocalStorage.email;
+
+  const profilePic = document.querySelector(".profile-pic");
+  profilePic.classList.toggle("hidden");
+} else {
+  signOut.classList.toggle("hidden");
+}
